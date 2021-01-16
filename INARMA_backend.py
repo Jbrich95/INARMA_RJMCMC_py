4cc77ab6a062011b5b2fd2e09a352cd1812e0a9b
Jbrich95 <j.flett@lancaster.ac.uk>
2021-01-16 14:04:35 +0000

    Fixed some errors

--- INARMA_backend.py
+++ INARMA_backend.py
@@ -20,21 +20,21 @@ def sim_aug_AR(x,y,z,alphas,lam,p,rmax):
         while True:
 
             for i in range(0,p):
-                yprop[t,i] = binom(x[t-i],alphas[i]).rvs()
+                yprop[t,i] = binom(n=x[t-i-1],p=alphas[i]).rvs()
 
-            zprop[t]=x[t]-sum(yprop[t,0:p-1])
+            zprop[t]=x[t]-sum(yprop[t,])
             if zprop[t]>=0: 
                 break
 
         uaug = np.random.rand()
-        dist=poisson(lam)
+        dist=poisson(mu=lam)
         A=dist.pmf(zprop[t])/dist.pmf(z[t])
         if uaug <= A :
-            y[t,] = np.copy(yprop[t,])
-            z[t,] = np.copy(zprop[t,])
+            y[t,] = yprop
+            z[t,] = zprop
 
-    out[:,0:p] = np.copy(y)
-    out[:,[p]] = np.copy(z)
+    out[:,0:p] = y
+    out[:,[p]] = z
         
     return(out)
     
@@ -52,30 +52,30 @@ def sim_aug_MA(x,v,z,betas,lam,q,rmax):
         while True:
 
             for i in range(0,q):
-                vprop[t,i] = binom(z[t-i],betas[i]).rvs()
+                vprop[t,i] = binom(n=z[t-i-1],p=betas[i]).rvs()
 
-            zprop[t]=x[t]-sum(vprop[t,0:q-1])
+            zprop[t]=x[t]-sum(vprop[t,])
             if zprop[t]>=0: 
                 break
 
         uaug = np.random.rand()
-        Q = np.min([q, n - t + 1])
-        dist=poisson(lam)
+        Q = np.min([q, n - t ])
+        dist=poisson(mu=lam)
         A=dist.pmf(zprop[t])/dist.pmf(z[t])
         if Q == 0:
             for i in range(0,Q):
-                distprop=binom(zprop[t], betas[i])
-                dist=binom(z[t], betas[i])
+                distprop=binom(n=zprop[t], p=betas[i])
+                dist=binom(n=z[t], p=betas[i])
                 np.seterr(invalid='ignore')
 
-                A *= distprop.pmf(v[t+i,i])/dist.pmf(v[t+i,i])
+                A *= distprop.pmf(v[t+i+1,i])/dist.pmf(v[t+i+1,i])
             
         if uaug <= A :
-            v[t,] = np.copy(vprop[t,])
-            z[t,] = np.copy(zprop[t,])
+            v[t,] = vprop
+            z[t,] = zprop
 
-    out[:,0:q] = np.copy(v)
-    out[:,[q]] = np.copy(z)
+    out[:,0:q] = v
+    out[:,[q]] = z
         
     return out
 
@@ -88,40 +88,40 @@ def sim_aug_ARMA(x,y,v,z,alphas,betas,lam,p,q,rmax):
     zprop = np.zeros([n,1])
     zprop[0:(rmax-1)] = x[0:(rmax-1)]
 
-    for t in range(rmax-1,n):
+    for t in range(rmax-1,n-1):
 
         while True:
 
             for i in range(0,p):
-                yprop[t,i] = binom(x[t-i],alphas[i]).rvs()
+                yprop[t,i] = binom(n=x[t-i-1],p=alphas[i]).rvs()
             for j in range(0,q):
-                vprop[t,j] = binom(int(z[t-j]),betas[j]).rvs()
-            zprop[t]=x[t]-sum(yprop[t,0:p-1])-sum(vprop[t,0:q-1])
+                vprop[t,j] = binom(n=int(z[t-j-1]),p=betas[j]).rvs()
+            zprop[t]=x[t]-sum(yprop[t,])-sum(vprop[t,])
             if zprop[t]>=0: 
                 break
 
         uaug = np.random.rand()
-        Q = np.min([q, n - t + 1])
-        dist=poisson(lam)
+        Q = np.min([q, n - t])
+        dist=poisson(mu=lam)
         A=dist.pmf(zprop[t])/dist.pmf(z[t])
         if Q == 0:
             for i in range(0,Q):
-                distprop=binom(zprop[t], betas[i])
-                dist=binom(z[t], beta[i])
+                distprop=binom(n=zprop[t], p=betas[i])
+                dist=binom(n=z[t], p=betas[i])
                 np.seterr(invalid='ignore')
 
-                A *= distprop.pmf(v[t+i,i])/dist.pmf(v[t+i,i])
+                A *= distprop.pmf(v[t+i+1,i])/dist.pmf(v[t+i+1,i])
             
         if uaug <= A :
-            v[t,] = np.copy(vprop[t,])
-            y[t,] = np.copy(yprop[t,])
+            v[t,] = vprop[t,]
+            y[t,] = yprop[t,]
 
-            z[t,] = np.copy(zprop[t,])
+            z[t,] =zprop[t,]
             
     
-    out[:,0:p] = np.copy(y)
-    out[:,(p):(p+q)] = np.copy(v)
-    out[:,(p+q):(p+q+1)] = np.copy(z)
+    out[:,0:p] = y
+    out[:,(p):(p+q)] = v
+    out[:,(p+q):(p+q+1)] = z
     
     return out
 
@@ -131,11 +131,11 @@ def sim_pars_AR(x,y,z,p):
     alpha = np.zeros([p,1])
     while True:
         for i in range(0,p):
-            dist = beta(a=sum(y[(p+1):n,i])+1,b=sum(x[(p+1-i):(n-i)])-sum(y[(p+1):n,i])+1)
+            dist = beta(a=sum(y[p:n,i])+1,b=sum(x[(p-i):(n-i)])-sum(y[(p):n,i])+1)
             alpha[i]= dist.rvs(1)
         if sum(alpha) < 1:
             break
-    lam=gamma(sum(z[(p+1):n])+1,n+1).rvs()
+    lam=gamma(a=sum(z[p:n])+1,scale=1/(n+1)).rvs()
     return alpha,lam
 
 def sim_pars_MA(x,v,z,q):
@@ -144,12 +144,12 @@ def sim_pars_MA(x,v,z,q):
     while True:
 
         for i in range(0,q):
-            dist = beta(a=sum(v[(q+1):n,i])+1,b=sum(z[(q+1-i):(n-i)])-sum(v[(q+1):n,i])+1)
+            dist = beta(a=sum(v[q:n,i])+1,b=sum(z[(q-i):(n-i)])-sum(v[q:n,i])+1)
             beta_sample[i]= dist.rvs(1)
         if sum(beta_sample) < 1: 
             break
   
-    lam=gamma(sum(z[(q+1):n])+1,n-q+1).rvs()
+    lam=gamma(a=sum(z[q:n])+1,scale=1/(n-q+1)).rvs()
     return beta_sample,lam
 
 
@@ -162,7 +162,7 @@ def sim_pars_ARMA(x,y,v,z,p,q):
     while True:
 
         for i in range(0,p):
-            dist = beta(a=sum(y[(p+1):n,i])+1,b=sum(x[(p+1-i):(n-i)])-sum(y[(p+1):n,i])+1)
+            dist = beta(a=sum(y[p:n,i])+1,b=sum(x[(p-i):(n-i)])-sum(y[p:n,i])+1)
             alpha[i]= dist.rvs(1)
         if sum(alpha) < 1: 
             break
@@ -170,12 +170,12 @@ def sim_pars_ARMA(x,y,v,z,p,q):
     while True:
 
         for i in range(0,q):
-            dist = beta(a=sum(v[(q+1):n,i])+1,b=sum(z[(q+1-i):(n-i)])-sum(v[(q+1):n,i])+1)
+            dist = beta(a=sum(v[q:n,i])+1,b=sum(z[(q-i):(n-i)])-sum(v[q:n,i])+1)
             beta_sample[i]= dist.rvs(1)
         if sum(beta_sample) < 1: 
             break
   
-    lam=gamma(sum(z[(q+1):n])+1,n-q+1).rvs()
+    lam=gamma(a=sum(z[q:n])+1,scale=1/(n-q+1)).rvs()
     return alpha,beta_sample,lam
 
 
@@ -199,23 +199,23 @@ def p_up(x_data,y,alphas,p,rmax):
     S=np.zeros(n)
     for t in range(0,n):
 
-        S[t]=binom(y[t,K],U).rvs(1)
+        S[t]=binom(n=y[t,K],p=U).rvs(1)
 
     yprop[:,K]= S
     yprop[:,pprop-1]=y[:,K]-S
 
-    prob=1
+    logprob=0
 
     for t in range(rmax-1,n):
-        prob *= binom(x_data[t-K-1],alphaprop[K]).pmf(yprop[t,K])*binom(x_data[t-pprop-1],alphaprop[pprop-1]).pmf(yprop[t,pprop-1])/(binom(x_data[t-K],alphas[K]).pmf(yprop[t,K])*binom(y[t,K],U).pmf(yprop[t,K]))
-    f=prob*pprop*n**(-0.5)
+        logprob += binom(n=x_data[t-K-1],p=alphaprop[K]).logpmf(yprop[t,K])+binom(n=x_data[t-pprop-1],p=alphaprop[pprop-1]).logpmf(yprop[t,pprop-1])-binom(n=x_data[t-K-1],p=alphas[K]).logpmf(yprop[t,K])-binom(n=y[t,K],p=U).logpmf(yprop[t,K])
+    logf=logprob+np.log(pprop)-0.5*np.log(n)
 
 
     J=alphas[K]  
 
-    A=f*J
+    logA=logf+np.log(J)
 
-    if np.random.rand(1) <= A:
+    if np.log(np.random.rand()) <= logA:
         p=pprop
         alphas=alphaprop
         y=yprop
@@ -234,16 +234,16 @@ def p_down(x_data,y,alphas,p,rmax):
     yprop[:,K]=y[:,K]+y[:,p-1]
     U = alphas[K]/(alphaprop[K])
 
-    prob=1
+    logprob=0
     for t in range(rmax-1,n):
-        prob *= binom(x_data[t-K-1],alphaprop[K]).pmf(yprop[t,K-1])/(binom(x_data[t-K-1],alphas[K]).pmf(y[t,K])*binom(x_data[t-p],alphas[p-1]).pmf(y[t,p-1]))*binom(yprop[t,K],U).pmf(y[t,K])
+        logprob += binom(n=x_data[t-K-1],p=alphaprop[K]).logpmf(yprop[t,K-1])-binom(n=x_data[t-K-1],p=alphas[K]).logpmf(y[t,K])-binom(n=x_data[t-p],p=alphas[p-1]).logpmf(y[t,p-1])-binom(n=yprop[t,K],p=U).logpmf(y[t,K])
 
-    J=1/alphaprop[K]  
-    f=prob*n**(0.5)/p
-    A=f*J
+    J = 1/alphaprop[K]  
+    logf=logprob+0.5*np.log(n)-np.log(p)
+    logA=logf+np.log(J)
 
     
-    if np.random.rand(1) <= A:
+    if np.log(np.random.rand(1)) <= logA:
         p=pprop
         alphas=alphaprop
         y=yprop
@@ -271,22 +271,21 @@ def arma_to_ma(x_data,y,v,z,alphas,betas,lam,p,q,rmax):
 
     U = alphas[0]
 
-    prob=1
+    logprob=0
     for t in range(rmax-1,n):
-        prob *= poisson(lamprop).pmf(zprop[t])/(poisson(lam).pmf(z[t])*binom(x_data[t-1],alphas[0]).pmf(y[t,0]))
-        prob *= binom(zprop[t],U).pmf(S[t,0])
-        prob *= 1/(multinom(y[t,0],gamma).pmf(S[t,]))
+        logprob += poisson(mu=lamprop).logpmf(zprop[t])-poisson(mu=lam).logpmf(z[t])+binom(n=x_data[t-1],p=alphas[0]).logpmf(y[t,0])
+        logprob += binom(n=zprop[t],p=U).logpmf(S[t,0])-multinom(n=y[t,0],p=gamma).logpmf(S[t,])
         for j in range(0,q):
-            prob *= binom(zprop[t-j-1],betas[j]).pmf(vprop[t,j])/binom(z[t - j - 1],betas[j]).pmf(v[t,j])
-            prob *=binom(vprop[t,j],U).pmf(S[t,j+1])
+            logprob += binom(n=zprop[t-j-1],p=betas[j]).logpmf(vprop[t,j])-binom(n=z[t - j - 1],p=betas[j]).logpmf(v[t,j])
+            logprob +=binom(n=vprop[t,j],p=U).logpmf(S[t,j+1])
 
-    f=prob*(n**(0.5))
+    logf=logprob+0.5*np.log(n)
 
     J=1/(1-U)  
 
-    A=f*J
+    logA=logf+np.log(J)
     
-    if np.random.rand(1) <= A:
+    if np.log(np.random.rand()) <= logA:
         p=0
         lam = lamprop
         y=0
@@ -305,8 +304,8 @@ def ma_to_arma(x_data, y, v,z,alphas, betas, lam,p, q,rmax)   :
     for t in range(0,n):
         for j in range(1,q+1):
 
-            S[t,j]=binom(v[t,j-1],U).rvs(1)
-        S[t,1]=binom(z[t],U).rvs(1)
+            S[t,j]=binom(n=v[t,j-1],p=U).rvs(1)
+        S[t,1]=binom(n=z[t],p=U).rvs(1)
 
 
     vprop = np.zeros([n,q])
@@ -319,7 +318,7 @@ def ma_to_arma(x_data, y, v,z,alphas, betas, lam,p, q,rmax)   :
         for j in range(0,q):
             vprop[t,j]=v[t,j] - S[t,j-1]
 
-    prob=1
+    logprob=1
     gamma = np.ones(q+1)
 
     for i in range(1,q+1) :
@@ -327,18 +326,18 @@ def ma_to_arma(x_data, y, v,z,alphas, betas, lam,p, q,rmax)   :
     gamma[0]=1/(sum(betas)+1)
 
     for t in range(rmax-1,n):
-        prob *= poisson(lamprop).pmf(zprop[t])*binom(x_data[t-1],alphaprop).pmf(yprop[t])/poisson(lam).pmf(z[t])
-        prob *=multinom(yprop[t],gamma).pmf(S[t,])
+        logprob += poisson(mu=lamprop).logpmf(zprop[t])+binom(n=x_data[t-1],p=alphaprop).logpmf(yprop[t])-poisson(mu=lam).logpmf(z[t])
+        logprob +=multinom(n=yprop[t],p=gamma).logpmf(S[t,])
         for j in range(0,q):
-            prob *= binom(zprop[t-j-1],betas[j]).pmf(vprop[t,j])/ binom(zprop[t-j-1],betas[j]).pmf(v[t,j])
-            prob *=1/binom(z[t],U).pmf(S[t,0])
+            logprob += binom(n=zprop[t-j-1],p=betas[j]).logpmf(vprop[t,j])-binom(n=zprop[t-j-1],p=betas[j]).logpmf(v[t,j])
+            logprob += -binom(n=z[t],p=U).logpmf(S[t,0])
 
-    f=prob*n**(-0.5)
+    logf=logprob-0.5*np.log(n)
 
     J=(1-U)
 
-    A=prob*J
-    if np.random.rand(1) <= A:
+    logA=logf+np.log(J)
+    if np.log(np.random.rand(1)) <= logA:
         p=1
         alphas=alphaprop
         y=yprop
